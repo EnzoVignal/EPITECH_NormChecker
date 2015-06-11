fc9f8a2ed2699778efd599bc9d8b801fd089eee0
Loïc Pirez <loic.pirez@epitech.eu>
2015-06-11 20:00:53 +0200

    Fix zsh env and conditions

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/sh
 
 cp -r mouli ~/.mouli
-if [ $SHELL == "/bin/zsh" ]
+if [ $SHELL == "/bin/zsh" ] || [ $SHELL == "/usr/bin/zsh" ]
 then
-   echo "alias norme='clear && cp ~/.mouli/norme.py ./; cp ~/.mouli/norme_auto.sh ./ && ./norme_auto.sh && rm -f norme.py norme_auto.sh'" >> ~/.zshrc
-elif [ $SHELL == "/bin/bash" ]
+    echo "alias norme='clear && cp ~/.mouli/norme.py ./; cp ~/.mouli/norme_auto.sh ./ && ./norme_auto.sh && rm -f norme.py norme_auto.sh'" >> ~/.zshrc
+echo "Install complete. Now source ~/.zshrc."
+elif [ $SHELL == "/bin/bash" ] || [ $SHELL == "/usr/bin/bash" ]
 then
    echo "alias norme='clear && cp ~/.mouli/norme.py ./; cp ~/.mouli/norme_auto.sh ./ && ./norme_auto.sh && rm -f norme.py norme_auto.sh'" >> ~/.bashrc
+echo "Install complete. Now source ~/.bashrc."
 else
 echo "Can't install Epi-Norme with your shell."
 fi
-echo "Install complete. Now source ~/.zshrc or ~/.bashrc."
